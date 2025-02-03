773c9e70228726e299430d269a4e4af540216989
Cristian Carvajal <crusale2015@gmail.com>
2025-02-03 15:13:47 -0500

    prueba 3

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { itemService } from './item.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('item')
 export class itemController {
   constructor(private readonly itemService: itemService) {}
 
   @Get()
   async getAllItems(@Req() req) {
-    return this.itemService.getAllItems();
+    return this.itemService.getAvailableItems(req.user.id);
   }
 }
