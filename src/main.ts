0394b8ad24638d52df083846cba0d2fa05914eca
Cristian Carvajal <crusale2015@gmail.com>
2025-02-03 08:01:15 -0500

    arreglo funcionamiento front desplegado

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://bookbit-mauve.vercel.app'], // Permite estos dominios
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   await app.listen(4000);
