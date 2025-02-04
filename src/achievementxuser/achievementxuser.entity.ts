30d3afc23091a6920a5d254803868b60aeb52edf
Cristian Carvajal <crusale2015@gmail.com>
2025-02-04 12:46:58 -0500

    Ligeros cambios al sistema de retos

--- src/achievementxuser/achievementxuser.entity.ts
+++ src/achievementxuser/achievementxuser.entity.ts
@@ -4,9 +4,11 @@ import {
   ManyToOne,
   CreateDateColumn,
   Column,
+  JoinColumn,
 } from 'typeorm';
 import { User } from '../users/user.entity';
 import { Achievement } from 'src/achievement/achievement.entity';
+import { State } from 'src/state/state.entity';
 
 @Entity()
 export class AchievementxUser {
@@ -24,4 +26,11 @@ export class AchievementxUser {
 
   @Column({ default: 0 })
   progress: number;
+
+  @Column({ default: 0 })
+  percentage: number;
+
+  @ManyToOne(() => State, { eager: true })
+  @JoinColumn({ name: 'state_id' })
+  state: State;
 }
